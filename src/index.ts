a57abb439fc0614cdbd6a0c76c4c3e761f9581a5
Rillmo <maxman306@gmail.com>
2026-06-27 11:21:05 +0900

    feat: export renderCardNews and types as public API

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export { renderCardNews } from "./tools/render-card-news.js";
+export type {
+  RenderCardNewsInput,
+  RenderedCard,
+} from "./tools/render-card-news.js";
+export { loadTheme } from "./renderer/theme-loader.js";
+export { createStorage } from "./storage/index.js";
+export type { StorageBackend, StorageResult } from "./storage/index.js";
